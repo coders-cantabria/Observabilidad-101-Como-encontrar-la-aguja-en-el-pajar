d9662c76e579e7847a7df63f5daeca9849e36036
Juan Toca <elan17.programacion@gmail.com>
2025-03-19 22:21:11 +0100

    Fixed metric auto-instrumentation

--- ClassQuiz/run-fastapi.sh
+++ ClassQuiz/run-fastapi.sh
@@ -4,4 +4,4 @@ OTEL_EXPORTER_OTLP_PROTOCOL=http/protobuf \
 OTEL_PYTHON_LOGGING_AUTO_INSTRUMENTATION_ENABLED="true" \
 OTEL_PYTHON_LOG_CORRELATION="true" \
 OTEL_PYTHON_DISABLED_INSTRUMENTATIONS="fastapi" \
-opentelemetry-instrument --logs_exporter otlp pipenv run uvicorn classquiz:app --proxy-headers
+opentelemetry-instrument --logs_exporter otlp --metrics_exporter otlp pipenv run uvicorn classquiz:app --proxy-headers
